fce03e44185bef45849e48cb2cb61e56c059e7ff
tabat <tabata.bran@gmail.com>
2020-11-29 16:53:13 -0300

    CriarRepublicaPresenter consertada

---
+++ Republica/src/main/java/presenter/CriarRepublicaPresenter.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package presenter;
+
+import view.CriarRepublicaView;
+
+/**
+ *
+ * @author tabat
+ */
+public class CriarRepublicaPresenter {
+    private CriarRepublicaView view;
+
+    public CriarRepublicaPresenter() {
+        this.view = new CriarRepublicaView();
+        this.view.setVisible(true);
+    }
+    
+    
+    
+    
+}
